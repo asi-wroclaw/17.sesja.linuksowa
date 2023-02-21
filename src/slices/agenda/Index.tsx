cfd740473110ca4c2301ab09f5e265899e798f96
jBosak <jbosak98@gmail.com>
2023-02-21 17:09:00 +0100

    feat: agenda style improvements

--- src/slices/agenda/Index.tsx
+++ src/slices/agenda/Index.tsx
@@ -1,7 +1,63 @@
 import { Box, VStack, Heading } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DaySelect from "./DaySelect";
 import Speech from "./Speech";
+
+const saturdaySpeeches = [
+  {
+    start: "9:00",
+    end: "9:50",
+    title: "Własny serwer email - jak i dlaczego [pl]1",
+    author: "Błażej Święcicki",
+  },
+  {
+    start: "10:00",
+    end: "10:50",
+    title: "Własny serwer email - jak i dlaczego [pl]2",
+    author: "Błażej Święcicki",
+  },
+  {
+    start: "11:00",
+    end: "11:50",
+    title: "Własny serwer email - jak i dlaczego [pl]3",
+    author: "Błażej Święcicki",
+  },
+  {
+    start: "12:00",
+    end: "12:50",
+    title: "Własny serwer email - jak i dlaczego [pl]4",
+    author: "Błażej Święcicki",
+  },
+  {
+    start: "13:00",
+    end: "14:00",
+    title: "Przerwa obiadowa",
+  },
+  {
+    start: "14:00",
+    end: "14:50",
+    title: "Własny serwer email - jak i dlaczego [pl]5",
+    author: "Błażej Święcicki",
+  },
+  {
+    start: "15:00",
+    end: "15:50",
+    title: "Własny serwer email - jak i dlaczego [pl]6",
+    author: "Błażej Święcicki",
+  },
+  {
+    start: "16:00",
+    end: "16:50",
+    title: "Własny serwer email - jak i dlaczego [pl]7",
+    author: "Błażej Święcicki",
+  },
+  {
+    start: "17:00",
+    end: "17:50",
+    title: "Własny serwer email - jak i dlaczego [pl]8",
+    author: "Błażej Święcicki",
+  },
+];
 
 const Agenda = () => {
   const [isSaturday, setSaturday] = useState<boolean>(true);
@@ -13,69 +69,17 @@ const Agenda = () => {
     {
       text: content.saturdayOption,
       select: () => setSaturday(true),
+
       selected: isSaturday,
     },
     {
       text: content.sundayOption,
       select: () => setSaturday(false),
+
       selected: !isSaturday,
     },
   ];
-  const speeches = [
-    {
-      start: "9:00",
-      end: "9:50",
-      title: "Własny serwer email - jak i dlaczego [pl]",
-      author: "Błażej Święcicki",
-    },
-    {
-      start: "10:00",
-      end: "10:50",
-      title: "Własny serwer email - jak i dlaczego [pl]",
-      author: "Błażej Święcicki",
-    },
-    {
-      start: "11:00",
-      end: "11:50",
-      title: "Własny serwer email - jak i dlaczego [pl]",
-      author: "Błażej Święcicki",
-    },
-    {
-      start: "12:00",
-      end: "12:50",
-      title: "Własny serwer email - jak i dlaczego [pl]",
-      author: "Błażej Święcicki",
-    },
-    {
-      start: "13:00",
-      end: "14:00",
-      title: "Przerwa obiadowa",
-    },
-    {
-      start: "14:00",
-      end: "14:50",
-      title: "Własny serwer email - jak i dlaczego [pl]",
-      author: "Błażej Święcicki",
-    },
-    {
-      start: "15:00",
-      end: "15:50",
-      title: "Własny serwer email - jak i dlaczego [pl]",
-      author: "Błażej Święcicki",
-    },
-    {
-      start: "16:00",
-      end: "16:50",
-      title: "Własny serwer email - jak i dlaczego [pl]",
-      author: "Błażej Święcicki",
-    },
-    {
-      start: "17:00",
-      end: "17:50",
-      title: "Własny serwer email - jak i dlaczego [pl]",
-      author: "Błażej Święcicki",
-    },
-  ];
+
   return (
     <Box as="section" zIndex="2" width="100%" bg="#000000" minHeight="100vh">
       <VStack gap="10" width="inherit">
@@ -88,8 +92,8 @@ const Agenda = () => {
           Agenda
         </Heading>
         <DaySelect days={days} />
-        <Box width="75%" maxW="40rem">
-          {speeches.map((speech) => (
+        <Box width={["95%", "80%", "70%", "60%"]} zIndex="30" margin="auto">
+          {saturdaySpeeches.map((speech) => (
             <Speech key={speech.title} speech={speech} />
           ))}
         </Box>
