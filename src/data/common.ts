95a1191358dd9d4777a2d3f949bb1502e54d3f37
Michał Korczak <michal.k2001@gmail.com>
2025-01-04 02:50:30 +0100

    refactor

--- src/data/common.ts
+++ src/data/common.ts
@@ -1,3 +1,5 @@
+export const callForPapersUrl = '';
+
 export const youtubeVideoId = 'r2FVjVojHDE';
 export const youtubePlaylistId = 'PLTvZBPQxCI8HIntC3SGEDJgAcnd3kMI-0';
 export const youtubePlaylistUrl = `https://www.youtube.com/watch?v=${youtubeVideoId}&list=${youtubePlaylistId}`;
