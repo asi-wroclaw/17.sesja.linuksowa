119a274eaed063ef8195951645c6aedd9a73de2a
Michał Korczak <michal.k2001@gmail.com>
2025-08-10 03:24:41 +0000

    feat: nice

--- src/data/common.ts
+++ src/data/common.ts
@@ -2,6 +2,6 @@ export const callForPapersUrl = '';
 
 export const liveUrl = 'https://youtube.com/live/#';
 
-export const youtubeVideoId = 'r2FVjVojHDE';
+export const youtubeVideoId = 'ORxQygAcUek';
 export const youtubePlaylistId = 'PLTvZBPQxCI8HIntC3SGEDJgAcnd3kMI-0';
 export const youtubePlaylistUrl = `https://www.youtube.com/watch?v=${youtubeVideoId}&list=${youtubePlaylistId}`;
